7c72b093da46e3acf9c1506868bb14c33bca86bb
Tobias Brinke <tobi.brinke@gmail.com>
2023-02-25 18:57:38 +0100

    INK - Basic Tests

---
+++ code/Innkeep/Innkeep.Core/Utilities/ClassDebugger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Innkeep.Core.Utilities;
+
+public static class ClassDebugger
+{
+    public static string CreateDebugString<T>(T obj)
+    {
+        var sb = new StringBuilder();
+        
+        foreach (var property in typeof(T).GetProperties())
+        {
+            var value = property.GetValue(obj);
+
+            if (property.PropertyType != typeof(string) && value is IEnumerable enumerable)
+            {
+                sb.AppendLine($"{property.Name}:");
+                
+                foreach (var item in enumerable)
+                {
+                    sb.AppendLine("\t" + item);
+                }
+                continue;
+            }
+            
+            sb.AppendLine($"{property.Name}: {property.GetValue(obj)}");
+        }
+        
+        sb.AppendLine(new string('-', 50));
+
+        return sb.ToString();
+    }
+}
---
+++ code/Innkeep/Innkeep.Data.Tests/Repositories/PretixRepositoryTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using Innkeep.Core.Interfaces.Pretix;
+using Innkeep.DI;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Innkeep.Data.Tests.Repositories;
+
+[TestClass]
+public class PretixRepositoryTests
+{
+    [TestInitialize]
+    public void TestInit()
+    {
+        DependencyManager.Initialize();
+    }
+    
+    [TestMethod]
+    public void GetOrganizers()
+    {
+        var repo = DependencyManager.ServiceProvider.GetRequiredService<IPretixRepository>();
+        
+        var response = Task.Run(() => repo.GetOrganizers()).Result;
+        
+        Trace.WriteLine(response);
+    }
+
+    [TestMethod]
+    public void GetEvents()
+    {
+        var repo = DependencyManager.ServiceProvider.GetRequiredService<IPretixRepository>();
+        
+        var organizer = Task.Run(() => repo.GetOrganizers()).Result.First();
+
+        var events = Task.Run(() => repo.GetEvents(organizer)).Result;
+
+        foreach (var e in events)
+        {
+            Trace.WriteLine(e);
+        }
+    }
+    
+    [TestMethod]
+    public void GetItems()
+    {
+        var repo = DependencyManager.ServiceProvider.GetRequiredService<IPretixRepository>();
+        
+        var organizer = Task.Run(() => repo.GetOrganizers()).Result.First();
+
+        var events = Task.Run(() => repo.GetEvents(organizer)).Result;
+
+        var items = Task.Run(() => repo.GetItems(organizer, events.First())).Result;
+
+        foreach (var e in items)
+        {
+            Trace.WriteLine(e);
+        }
+    }
+}
