eaa3f14e0414c8166a018a5cd2a27d11d7e4bb16
Tobias Brinke <tobi.brinke@gmail.com>
2023-04-29 15:25:38 +0200

    INK - Printer Library

---
+++ code/Innkeep/Innkeep.Printer/Document/DocumentManager.cs
@@ -0,0 +1,127 @@
+﻿using ESCPOS;
+using ESCPOS.Utils;
+using static ESCPOS.Commands;
+using System.Drawing;
+
+namespace Innkeep.Printer.Document;
+
+public class DocumentManager
+{
+	private byte[] _commands = Array.Empty<byte>();
+	private string _port;
+	
+	public DocumentManager(string port)
+	{
+		_port = port;
+	}
+
+	public DocumentManager AddTitle(string title, 
+				Justification justification = Justification.Center,
+				CharSizeWidth width = CharSizeWidth.Double,
+				CharSizeHeight height = CharSizeHeight.Double
+				)
+	{
+		Append(SelectJustification(justification));
+		Append(SelectCharSize(width, height));
+		Append(title.ToBytes());
+		ResetFontAndJustification();
+		Append(LF);
+
+		return this;
+	}
+
+	public DocumentManager AddLine(string lineText, 
+		Justification justification = Justification.Left)
+	{
+		Append(lineText.ToBytes());
+		Append(LF);
+		ResetJustification();
+		return this;
+	}
+
+	public DocumentManager AddEmptyLine()
+	{
+		Append(LF);
+		return this;
+	}
+
+	public DocumentManager AddQrCode(string content)
+	{
+		Append(SelectJustification(Justification.Center));
+		Append(PrintQRCode(content));
+		Append(LF);
+
+		return this;
+	}
+
+	public DocumentManager AddImage(string path)
+	{
+		Append(CreateImageArray(new Bitmap(path)));
+		Append(LF);
+
+		return this;
+	}
+
+	public DocumentManager Cut()
+	{
+		Append(LF, 5);
+		Append(FullPaperCut);
+		return this;
+	}
+
+	public DocumentManager PrintAndContinue()
+	{
+		_commands.Print(_port);
+		return this;
+	}
+
+	public void Print()
+	{
+		_commands.Print(_port);
+		_commands = Array.Empty<byte>();
+	}
+
+	private void ResetFontAndJustification()
+	{
+		ResetFont();
+		ResetJustification();
+	}
+
+	private void ResetFont()
+	{
+		Append(SelectCharSize(CharSizeWidth.Normal, CharSizeHeight.Normal));
+	}
+
+	private void ResetJustification()
+	{
+		Append(SelectJustification(Justification.Left));
+	}
+
+	private byte[] CreateImageArray(Bitmap image)
+	{
+		var width = image.Width;
+		var height = image.Height;
+
+		if (width > 384)
+		{
+			var sizeDivider = (int)Math.Ceiling(image.Width / 384d);
+			image = new Bitmap(image, new Size(width / sizeDivider, height / sizeDivider));
+		}
+		
+		var bytes = new Image.Image();
+
+		var byteArray = bytes.Print(image);
+
+		return byteArray;
+	}
+
+	private void Append(byte[] command, int count = 1)
+	{
+		for (var i = 0; i < count; i++)
+		{
+			_commands = _commands.Add(command);
+		}
+	}
+	
+	
+}
---
+++ code/Innkeep/Innkeep.Printer/Image/Image.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Drawing;
+
+namespace Innkeep.Printer.Image;
+
+public class Image
+    {
+        private static BitmapData GetBitmapData(Bitmap bmp)
+        {
+  
+                var threshold = 127;
+                var index = 0;
+                double multiplier = 576; // this depends on your printer model.
+                double scale = (double)(multiplier / (double)bmp.Width);
+                int xheight = (int)(bmp.Height * scale);
+                int xwidth = (int)(bmp.Width * scale);
+                var dimensions = xwidth * xheight;
+                var dots = new BitArray(dimensions);
+
+                for (var y = 0; y < xheight; y++)
+                {
+                    for (var x = 0; x < xwidth; x++)
+                    {
+                        var _x = (int)(x / scale);
+                        var _y = (int)(y / scale);
+                        var color = bmp.GetPixel(_x, _y);
+                        var luminance = (int)(color.R * 0.3 + color.G * 0.59 + color.B * 0.11);
+                        dots[index] = (luminance < threshold);
+                        index++;
+                    }
+                }
+
+                return new BitmapData()
+                {
+                    Dots = dots,
+                    Height = (int)(bmp.Height * scale),
+                    Width = (int)(bmp.Width * scale)
+                };
+       
+        }
+
+        public byte[] Print(Bitmap image)
+        {
+            var data = GetBitmapData(image);
+            BitArray dots = data.Dots;
+            byte[] width = BitConverter.GetBytes(data.Width);
+
+            int offset = 0;
+            MemoryStream stream = new MemoryStream();
+            BinaryWriter bw = new BinaryWriter(stream);
+
+            bw.Write((char)0x1B);
+            bw.Write('@');
+
+            bw.Write((char)0x1B);
+            bw.Write('3');
+            bw.Write((byte)24);
+
+            while (offset < data.Height)
+            {
+                bw.Write((char)0x1B);
+                bw.Write('*');         // bit-image mode
+                bw.Write((byte)33);    // 24-dot double-density
+                bw.Write(width[0]);  // width low byte
+                bw.Write(width[1]);  // width high byte
+
+                for (int x = 0; x < data.Width; ++x)
+                {
+                    for (int k = 0; k < 3; ++k)
+                    {
+                        byte slice = 0;
+                        for (int b = 0; b < 8; ++b)
+                        {
+                            int y = (((offset / 8) + k) * 8) + b;
+                            // Calculate the location of the pixel we want in the bit array.
+                            // It'll be at (y * width) + x.
+                            int i = (y * data.Width) + x;
+
+                            // If the image is shorter than 24 dots, pad with zero.
+                            bool v = false;
+                            if (i < dots.Length)
+                            {
+                                v = dots[i];
+                            }
+                            slice |= (byte)((v ? 1 : 0) << (7 - b));
+                        }
+
+                        bw.Write(slice);
+                    }
+                }
+                offset += 24;
+                bw.Write((char)0x0A);
+            }
+            // Restore the line spacing to the default of 30 dots.
+            bw.Write((char)0x1B);
+            bw.Write('3');
+            bw.Write((byte)30);
+
+            bw.Flush();
+            byte[] bytes = stream.ToArray();
+            bw.Dispose();
+            return bytes;
+        }
+    }
+
+    public class BitmapData
+    {
+        public BitArray Dots { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+    }
