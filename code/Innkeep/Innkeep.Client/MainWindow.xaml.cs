6478ddae96183d60ce750d6a477f89d0df70e3f3
Demolite <tobi.brinke@gmail.com>
2024-05-28 19:54:56 +0200

    WPF Hybrid Rework

---
+++ code/Innkeep/Innkeep.Client/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+
+namespace Innkeep.Client;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        var collection = new ServiceCollection();
+        collection.AddWpfBlazorWebView();
+        collection.AddBlazorWebViewDeveloperTools();
+        collection.AddMudServices();
+        
+        Resources.Add("services", collection.BuildServiceProvider());
+    }
+}
