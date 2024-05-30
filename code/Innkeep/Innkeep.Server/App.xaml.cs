d7fa3cf717ddad8b637d0ec95f3021cda6f21d57
Demolite <tobi.brinke@gmail.com>
2024-05-30 18:45:32 +0200

    Project Cleanup

--- code/Innkeep/Innkeep.Server/App.xaml.cs
+++ code/Innkeep/Innkeep.Server/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Innkeep.Server.Db.Context;
+using Innkeep.Startup.Database;
+using Innkeep.Startup.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
 
 namespace Innkeep.Server;
 
@@ -9,4 +13,22 @@ namespace Innkeep.Server;
 /// </summary>
 public partial class App : Application
 {
+	protected override void OnStartup(StartupEventArgs e)
+	{
+		base.OnStartup(e);
+		
+		var collection = new ServiceCollection();
+		collection.AddWpfBlazorWebView();
+		collection.AddBlazorWebViewDeveloperTools();
+		collection.AddMudServices();
+        
+		ServerServiceManager.ConfigureServices(collection);
+
+		var provider = collection.BuildServiceProvider();
+        
+		DatabaseCreator.EnsureDbCreated(provider.GetRequiredService<IDbContextFactory<InnkeepServerContext>>());
+
+		var mainWindow = new MainWindow(provider);
+		mainWindow.Show();
+	}
 }
---
+++ code/Innkeep/Innkeep.Startup/Database/DatabaseCreator.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Innkeep.Startup.Database;
+
+public class DatabaseCreator
+{
+	public static void EnsureDbCreated<T>(IDbContextFactory<T> factory) where T: DbContext
+	{
+		var db = factory.CreateDbContext();
+		db.Database.Migrate();
+	}
+}
