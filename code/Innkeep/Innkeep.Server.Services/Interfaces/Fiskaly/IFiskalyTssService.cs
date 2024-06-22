d2721c29afaeeb036911150c58b49073ff394b1e
Demolite <tobi.brinke@gmail.com>
2024-06-22 12:42:10 +0200

    Added Fiskaly TSS Selection

--- code/Innkeep/Innkeep.Server.Services/Interfaces/Fiskaly/IFiskalyTssService.cs
+++ code/Innkeep/Innkeep.Server.Services/Interfaces/Fiskaly/IFiskalyTssService.cs
@@ -5,6 +5,10 @@ namespace Innkeep.Server.Services.Interfaces.Fiskaly;
 public interface IFiskalyTssService
 {
 	public IEnumerable<FiskalyTss> TssObjects { get; set; }
+	
+	public FiskalyTss? CurrentTss { get; set; }
 
 	public Task Load();
+
+	public Task<bool> Save();
 }
