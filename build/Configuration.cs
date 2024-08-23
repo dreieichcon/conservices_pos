cb70c09e667c9b0b6d99b17da3bbaf16e5609a2c
Demolite <tobi.brinke@gmail.com>
2024-08-23 16:48:01 +0200

    Updated Build File to Export two Executables

--- build/Configuration.cs
+++ build/Configuration.cs
@@ -7,6 +7,7 @@ using Nuke.Common.Tooling;
 public class Configuration : Enumeration
 {
     public static Configuration Debug = new Configuration { Value = nameof(Debug) };
+    
     public static Configuration Release = new Configuration { Value = nameof(Release) };
 
     public static implicit operator string(Configuration configuration)
