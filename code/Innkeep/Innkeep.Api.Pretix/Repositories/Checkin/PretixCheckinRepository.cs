171705d8dbfaac05e366b82b7786cb728098624d
Demolite <tobi.brinke@gmail.com>
2024-09-22 13:00:38 +0200

    [Issue-3] Fixed Checkin Repository Issues

--- code/Innkeep/Innkeep.Api.Pretix/Repositories/Checkin/PretixCheckinRepository.cs
+++ code/Innkeep/Innkeep.Api.Pretix/Repositories/Checkin/PretixCheckinRepository.cs
@@ -1,11 +1,9 @@
 ﻿using Innkeep.Api.Auth;
 using Innkeep.Api.Endpoints.Pretix;
 using Innkeep.Api.Models.Pretix.Objects.Checkin;
-using Innkeep.Api.Models.Pretix.Response;
 using Innkeep.Api.Pretix.Interfaces.Checkin;
 using Innkeep.Api.Pretix.Repositories.Core;
 using Lite.Http.Interfaces;
-using Lite.Http.Response;
 
 namespace Innkeep.Api.Pretix.Repositories.Checkin;
 
@@ -15,9 +13,6 @@ public class PretixCheckinRepository(IPretixAuthenticationService authentication
 	public async Task<IHttpResponse<PretixCheckinResponse>> CheckIn(string organizerSlug, PretixCheckin checkin)
 	{
 		var uri = PretixUrlBuilder.Endpoints.Organizer(organizerSlug).Checkin();
-
-		var result = await Post<PretixCheckin, PretixResponse<PretixCheckinResponse>>(uri, checkin);
-
-		return HttpResponse<PretixCheckinResponse>.FromResult(result, x => x.Results.FirstOrDefault());
+		return await Post<PretixCheckin, PretixCheckinResponse>(uri, checkin);
 	}
 }
