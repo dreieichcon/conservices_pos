a166ee4b985f8273634edc6dda56310ce25fff37
Demolite <tobi.brinke@gmail.com>
2024-10-09 17:21:43 +0200

    [Issue-13] Cleaned up Startup Code for Client

--- code/Innkeep/Innkeep.Client/App.xaml.cs
+++ code/Innkeep/Innkeep.Client/App.xaml.cs
@@ -1,13 +1,7 @@
 ﻿using System.Windows;
 using Innkeep.Client.Startup;
-using Innkeep.Db.Client.Context;
-using Innkeep.Startup.Database;
 using Innkeep.Startup.Services;
 using Innkeep.Strings;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Serilog;
-using Velopack;
 
 namespace Innkeep.Client;
 
@@ -23,43 +17,6 @@ public partial class App : Application
 
 		base.OnStartup(e);
 
-		VelopackApp.Build().Run();
-
-		# if RELEASE
-		try
-		{
-			Task.Run(async () => await UpdateApp()).GetAwaiter().GetResult();
-
-			if (Environment.HasShutdownStarted)
-				return;
-		}
-		catch (Exception ex)
-		{
-			Log.Error(ex, "Error while checking for updates");
-		}
-		# endif
-
-		var host = KestrelBuilder.Build();
-
-		DatabaseCreator.EnsureDbCreated(host.Services.GetRequiredService<IDbContextFactory<InnkeepClientContext>>());
-
-		ClientEventInitializer.Initialize(host.Services);
-
-		var mainWindow = new MainWindow(host);
-		mainWindow.Show();
-	}
-
-	private static async Task UpdateApp()
-	{
-		var mgr = new UpdateManager("https://updates.conservices.de/innkeep/client");
-
-		var newVersion = await mgr.CheckForUpdatesAsync();
-
-		if (newVersion == null)
-			return;
-
-		await mgr.DownloadUpdatesAsync(newVersion);
-
-		mgr.ApplyUpdatesAndRestart(newVersion);
+		Bootstrapper.Run();
 	}
 }
---
+++ code/Innkeep/Innkeep.Client/Startup/Bootstrapper.cs
@@ -0,0 +1,64 @@
+﻿using Innkeep.Db.Client.Context;
+using Innkeep.Services.Client.Interfaces.Pos;
+using Innkeep.Startup.Database;
+using Innkeep.Startup.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Velopack;
+
+namespace Innkeep.Client.Startup;
+
+public static partial class Bootstrapper
+{
+	public static void Run()
+	{
+		// Setup Serilog for the Client
+		LoggingManager.InitializeLogger("Innkeep Client");
+
+		// Setup Velopack and Check for Updates
+		ConfigureUpdates();
+
+		// In case Velopack wants to quit the app to update
+		// abort the current startup
+		if (Environment.HasShutdownStarted)
+			return;
+
+		// Create the main application host
+		var host = CreateHost();
+
+		// Initialize the db and run migrations 
+		DatabaseCreator.EnsureDbCreated(host.Services.GetRequiredService<IDbContextFactory<InnkeepClientContext>>());
+
+		// Initialize service instances
+		InitializeServices(host);
+
+		// Start application
+		var mainWindow = new MainWindow(host);
+		mainWindow.Show();
+	}
+
+	/// <summary>
+	///     Enables Velopack and checks for updates.
+	/// </summary>
+	private static void ConfigureUpdates()
+	{
+		VelopackApp.Build()
+					.Run();
+
+# if RELEASE
+		Task.Run(async () => await InnkeepUpdater.CheckForUpdates("https://updates.conservices.de/innkeep/client"))
+			.GetAwaiter()
+			.GetResult();
+#endif
+	}
+
+	/// <summary>
+	///     Initializes services by instantiating them and performs other initialization functions.
+	/// </summary>
+	/// <param name="host">Current host, holding the services.</param>
+	private static void InitializeServices(IHost host)
+	{
+		host.Services.GetRequiredService<ISalesItemService>();
+	}
+}
