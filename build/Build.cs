d58434698cdbb9f44b48b3c8c992d7ecd0dbed81
Demolite <tobi.brinke@gmail.com>
2024-10-09 16:13:38 +0200

    [Issue-11] Created Velopack Build Script and Integrated Velopack

--- build/Build.cs
+++ build/Build.cs
@@ -1,117 +1,161 @@
+using System;
 using System.IO;
+using System.Management.Automation;
 using Nuke.Common;
-using Nuke.Common.CI;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tools.DotNet;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 class Build : NukeBuild
 {
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+	const string Version = "0.2.0";
 
-    [Solution(GenerateProjects = true)] 
-    readonly Solution Solution;
+	[Nuke.Common.Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+	readonly Configuration Configuration = Configuration.Release;
 
-    Project ServerProject => Solution.Ui.Server.Innkeep_Server;
-    
-    Project ClientProject => Solution.Ui.Client.Innkeep_Client;
+	/// Support plugins are available for:
+	/// - JetBrains ReSharper        https://nuke.build/resharper
+	/// - JetBrains Rider            https://nuke.build/rider
+	/// - Microsoft VisualStudio     https://nuke.build/visualstudio
+	/// - Microsoft VSCode           https://nuke.build/vscode
+	[Solution(GenerateProjects = true)]
+	readonly Solution Solution;
 
-    static AbsolutePath SourceDirectory => RootDirectory / "source";
+	Project ServerProject => Solution.Ui.Server.Innkeep_Server;
 
-    static AbsolutePath TestsDirectory => RootDirectory / "tests";
+	Project ClientProject => Solution.Ui.Client.Innkeep_Client;
 
-    static AbsolutePath OutputDirectory => RootDirectory / "release";
-    
-    static AbsolutePath OutputDirectoryServer => OutputDirectory / "server";
-    
-    static AbsolutePath OutputDirectoryClient => OutputDirectory / "client";
-    
-    public static int Main () => Execute<Build>(x => x.Publish);
+	static AbsolutePath SourceDirectory => RootDirectory / "source";
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = Configuration.Release;
+	static AbsolutePath TestsDirectory => RootDirectory / "tests";
 
-    Target Clean => definition => definition
-        .Before(Restore)
-        .Executes(() =>
-        {
-            foreach(var path in SourceDirectory.GlobDirectories("**/bin", "**/obj"))
-            {
-                path.DeleteDirectory();
-            }
-            
-            foreach(var path in TestsDirectory.GlobDirectories("**/bin", "**/obj"))
-            {
-                path.DeleteDirectory();
-            }
+	static AbsolutePath OutputDirectory => RootDirectory / "release";
 
-            OutputDirectory.CreateOrCleanDirectory();
-        });
+	static AbsolutePath OutputDirectoryServer => OutputDirectory / "server";
 
-    Target Restore => definition => definition
-        .DependsOn(Clean)
-        .Executes(() =>
-        {
-            DotNetRestore(s => s
-                .SetProjectFile(ServerProject));
-            
-            DotNetRestore(s => s
-                .SetProjectFile(ClientProject));
-        });
+	static AbsolutePath OutputDirectoryClient => OutputDirectory / "client";
 
-    Target Compile => definition => definition
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-            DotNetBuild(s => s
-                 .SetProjectFile(ServerProject)
-                 .SetConfiguration(Configuration)
-                 .EnableNoRestore()
-            );
-            
-            DotNetBuild(s => s
-                 .SetProjectFile(ClientProject)
-                 .SetConfiguration(Configuration)
-                 .EnableNoRestore()
-            );
-        });
+	static AbsolutePath BuildDirectoryClient => OutputDirectoryClient / "build";
 
-    Target Publish => definition => definition
-         .DependsOn(Compile)
-         .Executes(() =>
-             {
-                 DotNetPublish(s => s
-                    .SetProject(ServerProject)
-                    .SetOutput(OutputDirectoryServer)
-                    .SetConfiguration(Configuration.Release)
-                    .SetPublishSingleFile(true)
-                    .SetSelfContained(true)
-                    .SetPublishTrimmed(false)
-                 );
+	static AbsolutePath BuildDirectoryServer => OutputDirectoryServer / "build";
 
-                 Directory.CreateDirectory(Path.Combine(OutputDirectoryServer, "log"));
-                 Directory.CreateDirectory(Path.Combine(OutputDirectoryServer, "cert"));
+	static AbsolutePath PackDirectoryClient => OutputDirectoryClient / "pack";
 
-                 DotNetPublish(s => s
-                                    .SetProject(ClientProject)
-                                    .SetOutput(OutputDirectoryClient)
-                                    .SetConfiguration(Configuration.Release)
-                                    .SetPublishSingleFile(true)
-                                    .SetSelfContained(true)
-                                    .SetPublishTrimmed(false)
-                 );
-                     
-                 Directory.CreateDirectory(Path.Combine(OutputDirectoryClient, "log"));
-                 Directory.CreateDirectory(Path.Combine(OutputDirectoryClient, "cert"));
-                 
-             }
-         );
+	static AbsolutePath PackDirectoryServer => OutputDirectoryServer / "pack";
 
+	Target Clean => definition => definition
+								.Before(Restore)
+								.Executes(() =>
+									{
+										foreach (var path in SourceDirectory.GlobDirectories("**/bin", "**/obj"))
+											path.DeleteDirectory();
+
+										foreach (var path in TestsDirectory.GlobDirectories("**/bin", "**/obj"))
+											path.DeleteDirectory();
+
+										BuildDirectoryClient.CreateOrCleanDirectory();
+										BuildDirectoryServer.CreateOrCleanDirectory();
+									}
+								);
+
+	Target Restore => definition => definition
+									.DependsOn(Clean)
+									.Executes(() =>
+										{
+											DotNetRestore(s => s
+												.SetProjectFile(ServerProject)
+											);
+
+											DotNetRestore(s => s
+												.SetProjectFile(ClientProject)
+											);
+										}
+									);
+
+	Target Compile => definition => definition
+									.DependsOn(Restore)
+									.Executes(() =>
+										{
+											DotNetBuild(s => s
+															.SetProjectFile(ServerProject)
+															.SetConfiguration(Configuration)
+															.EnableNoRestore()
+											);
+
+											DotNetBuild(s => s
+															.SetProjectFile(ClientProject)
+															.SetConfiguration(Configuration)
+															.EnableNoRestore()
+											);
+										}
+									);
+
+	Target Publish => definition => definition
+									.DependsOn(Compile)
+									.Executes(() =>
+										{
+											DotNetPublish(s => s
+																.SetProject(ServerProject)
+																.SetOutput(BuildDirectoryServer)
+																.SetConfiguration(Configuration.Release)
+																.SetPublishSingleFile(true)
+																.SetSelfContained(true)
+																.SetPublishTrimmed(false)
+											);
+
+											Directory.CreateDirectory(Path.Combine(BuildDirectoryServer, "log"));
+											Directory.CreateDirectory(Path.Combine(BuildDirectoryServer, "cert"));
+
+											DotNetPublish(s => s
+																.SetProject(ClientProject)
+																.SetOutput(BuildDirectoryClient)
+																.SetConfiguration(Configuration.Release)
+																.SetPublishSingleFile(true)
+																.SetSelfContained(true)
+																.SetPublishTrimmed(false)
+											);
+
+											Directory.CreateDirectory(Path.Combine(BuildDirectoryClient, "log"));
+											Directory.CreateDirectory(Path.Combine(BuildDirectoryClient, "cert"));
+										}
+									);
+
+	Target Pack => definition => definition
+								.DependsOn(Publish)
+								.Executes(() =>
+									{
+										Console.WriteLine("Packing Client");
+										PackClient();
+
+										Console.WriteLine("Packing Server");
+										PackServer();
+									}
+								);
+
+	static void PackClient()
+		=> PackInternal(OutputDirectoryClient, "Innkeep.Client");
+
+	static void PackServer()
+		=> PackInternal(OutputDirectoryServer, "Innkeep.Server");
+
+	static void PackInternal(AbsolutePath outputDirectory, string packId)
+	{
+		using var ps = PowerShell.Create();
+
+		var cdCommand = $"cd \"{outputDirectory}\"";
+
+		var packCommand =
+			$@"vpk pack --packId {packId} --packVersion {Version} --packDir .\build --mainExe {packId}.exe --outputDir .\pack";
+
+		ps.AddScript(cdCommand);
+		ps.AddScript(packCommand);
+
+		var results = ps.Invoke();
+
+		foreach (var result in results)
+			Console.WriteLine(result);
+	}
+
+	public static int Main() => Execute<Build>(x => x.Pack);
 }
