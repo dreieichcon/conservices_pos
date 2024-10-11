ee04a6244afa2ae63a522d09fec1d6c4ada0dda3
Demolite <tobi.brinke@gmail.com>
2024-10-11 15:01:44 +0200

    [Issue-16] Refactored Logging to use Env file

--- code/Innkeep/Innkeep.Startup/Services/LoggingManager.cs
+++ code/Innkeep/Innkeep.Startup/Services/LoggingManager.cs
@@ -1,5 +1,4 @@
-﻿using Innkeep.Startup.Authentication;
-using Serilog;
+﻿using Serilog;
 using Serilog.Events;
 using Serilog.Sinks.Discord;
 
@@ -9,20 +8,33 @@ public static class LoggingManager
 {
 	public static void InitializeLogger(string serviceName)
 	{
-		var auth = new Auth();
-		
-		Log.Logger = new LoggerConfiguration()
-					.WriteTo.Trace()
-					.WriteTo.Debug()
-					.WriteTo.Console()
-					.WriteTo.File("./log/log.txt", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel:LogEventLevel.Information)
-					.WriteTo.Discord(LogEventLevel.Warning, config =>
-					{
-						config.WebhookUrl = auth.WebhookUrl;
-						config.ServiceName = serviceName;
-					})
-					.Enrich.FromLogContext()
-					.MinimumLevel.Override("Microsoft.EntityFrameworkCore.Database.Command", LogEventLevel.Warning)
-					.CreateLogger();
+		var configuration = new LoggerConfiguration()
+							.WriteTo.Trace()
+							.WriteTo.Debug()
+							.WriteTo.Console()
+							.WriteTo.File(
+								"./log/log.txt",
+								rollingInterval: RollingInterval.Day,
+								restrictedToMinimumLevel: LogEventLevel.Information
+							)
+							.Enrich.FromLogContext()
+							.MinimumLevel.Override(
+								"Microsoft.EntityFrameworkCore.Database.Command",
+								LogEventLevel.Warning
+							);
+
+		var webhookUrl = Environment.GetEnvironmentVariable("DISCORD_WEBHOOK_URL");
+
+		if (webhookUrl is not null)
+			configuration.WriteTo.Discord(
+				LogEventLevel.Warning,
+				config =>
+				{
+					config.WebhookUrl = webhookUrl;
+					config.ServiceName = serviceName;
+				}
+			);
+
+		Log.Logger = configuration.CreateLogger();
 	}
 }
